5721e690605899e8cdebb9265d398ec56ad4573c
Miguel Méndez Martínez <miguetripleM@gmail.com>
2021-06-18 09:15:35 +0200

    busqueda de usuarios

--- credentials.py
+++ credentials.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+#creacion de la estructura que contiene los diferentes datos sobre la APY
 CREDENTIALS = {
   'API_KEY': os.environ['API_KEY'],
   'API_SECRET': os.environ['API_SECRET'],
--- main.py
+++ main.py
@@ -12,6 +12,13 @@ auth.set_access_token(CREDENTIALS['ACCESS_KEY'],
 api = tweepy.API(auth)
 
 try:
+    users = api.search_users("G2Caps", 20)
+    if users:
+        for u in users:
+            print("The user is " + str(u.name) + ".")
+        print("Fin de lista de busqueda de usuarios.\n")
+    else:
+        print("Error.")
     user = api.verify_credentials()
     if user:
         print("The user is " + str(user.location) + ".")
