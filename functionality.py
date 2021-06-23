23a3ec330a922c76fc25c08f5f45124525696f67
Miguel Méndez Martínez <miguetripleM@gmail.com>
2021-06-23 16:35:48 +0200

    deteccion de menciones y publicacion de respuesta

--- functionality.py
+++ functionality.py
@@ -1,5 +1,6 @@
 import time
 import constants
+import datetime
 import tweepy
 
 
@@ -33,13 +34,64 @@ def delete_portal(api):
         print("Tweet feed vacia no se podra borrar nada.")
         return False
 
+def responde_tweet(api, mencion):
+    now = datetime.now()
+
+    # formateado de los minutos
+    if now.minute < 10:
+        minute = '0' + str(now.minute)
+    else:
+        minute = str(now.minute)
+
+    # publicacion del tuit
+    status = api.update_status('@' + mencion.user.screen_name + 'yo tambien te quiero a dia' + str(now.day) + '/' + str(now.month) + '/' + str(now.year)  + ' a las ' + str(now.hour) + ':' + minute)
+
+    # comprobacion de si se ha publicado correctamente
+    if status:
+        time.sleep(15)
+        return True
+
+
 
 def menciones_Bot(api):
-    mentions_list = api.mentions_timeline(count=1)
-    if mentions_list:
-        for mention in mentions_list:
-            print(mention.user.screen_name + "te ha mencionado, tal que: " + mention.text + "\n")
-        return True
-    else:
-        return False
+    nombre_arq = "MencionesTw.txt"
+
+    mencionesAnteriores = []
+
+    nuevasMenciones = []
+
+    #aqui generamos la lista de menciones anteriores de manera que sepamos cuales ya pasaron
+    with open(nombre_arq) as arq:
+        for linea in arq:
+            linea = linea.strip("\n")
+            mencionesAnteriores.append(linea)
+
+    #añadimos al archivo la nueva mencion
+    with open(nombre_arq, "a") as arq:
+        for mention in api.mentions_timeline(count=1):
+            arq.write("@" + mention.user.screen_name + "te ha mencionado diciendo: " + mention.text +"\n")
+
+            #print(mention.user.screen_name + "te ha mencionado, tal que: " + mention.text + "\n")
+
+    #creamos la lista de nuevas menciones, que serán recorrer el archivo acutalizado
+    with open(nombre_arq) as arq:
+        for linea in arq:
+            linea = linea.strip("\n")
+            nuevasMenciones.append(linea)
+
+    #ahora comparamos ambas listas pasandolas a tipo set
+    setMencionesAnt = set(mencionesAnteriores)
+    setMencionesNuevas = set(nuevasMenciones)
+    diferencia = setMencionesNuevas.difference(setMencionesAnt)
+
+    #recorremos el nuevo set e imprimimos la nueva
+    for mencion in diferencia:
+        print(mention.text)
+        responde_tweet(api, mention)
+
+    time.sleep(15)
+
+
+
+
 
--- main.py
+++ main.py
@@ -17,41 +17,12 @@ api = tweepy.API(auth)
 
 print("Hola soy " + api.me().name + " un bot de twitter, me actualizo cada hora publicando un tweet pero tengo mas funciones que desea hacer:")
 
-#while True:
-#functionality.menciones_Bot(api)
-
 while True:
-    functionality.menciones_Bot(api)
-    print("""""
-       1.Consultar usuarios
-       2.Borrar feed
-       3.Publicar tweet
-       4.Exit/Quit
-       """"")
-    print("Tiempo restante para siguiente tweet automatico--> ")
+    try:
+        functionality.menciones_Bot(api)
+    except Exception as e:
+        print(e)
 
-    selection = input("Please Select:")
 
-    selection = 1
-
-    if selection == '1':
-        print()
-    elif selection == '2':
-        print("Eliminando feed")
-        try:
-            functionality.delete_portal(api)
-        except Exception as e:
-            print(e)
-    elif selection == '3':
-        print("Publicacion de tweet")
-        try:
-            while True:
-                functionality.post_tweet(api)
-        except Exception as e:
-            print(e)
-    elif selection == '4':
-        break
-    else:
-        print("Unknown Option Selected!")
 
 
